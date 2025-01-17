42a27a15254351fae926bb405cc03ac79b5776f7
jkulmus <jacquelynkulmus@gmail.com>
2025-01-17 13:52:20 -0800

    add

--- Week 2/ponder/dom-basics.js
+++ Week 2/ponder/dom-basics.js
@@ -1,3 +1,8 @@
 const newParagraph = document.createElement('p');
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos200");
+newImage.setAttribute("alt", "Random image from picsum.");
+document.body.appendChild(newImage);
