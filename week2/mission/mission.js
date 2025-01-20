a3c0a697293a9dfd1c94f1d7dd106c684b1477e5
jkulmus <jacquelynkulmus@gmail.com>
2025-01-20 13:46:42 -0800

    updated

--- week2/mission/mission.js
+++ week2/mission/mission.js
@@ -1,15 +1,14 @@
-const themeSelector = document.querySelector('#theme-selector');
-const body = document.body;
-const logo = document.getElementById('logo');
+document.addEventListener('DOMContentLoaded', function() { 
+    const themeSelector = document.querySelector('#themeSelector');
 
-function changeTheme() {
-    if (themeSelector.value === 'dark') {
-        body.classList.add('dark');
-        logo.src = 'byui-logo-white.webp';
-    } else {
-        body.classList.remove('dark');
-        logo.src = 'byui-logo.webp';
+    function changeTheme() {
+        if (themeSelector.value === 'dark') {
+            document.body.classList.add('dark');
+            document.querySelector('.logo').src = 'logo-white.webp'; 
+        } else { document.body.classList.remove('dark');
+            document.querySelector('.logo').src = 'logo.webp';
+        } 
     }
-}
-
-themeSelector.addEventListener('change', changeTheme);
+    
+    themeSelector.addEventListener('change', changeTheme);
+});
