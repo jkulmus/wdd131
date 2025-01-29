3df4ac9e90a8c51fee7e73a7fb8304fa1690582f
jkulmus <jacquelynkulmus@gmail.com>
2025-01-29 15:54:28 -0800

    update2

--- week4/debugging.js
+++ week4/debugging.js
@@ -1,6 +1,8 @@
-const pi = 3.14;
-const radius = 3;
+const PI = 3.14;
+let radius = 3;
 let area = 0;
-area = radius * radius * pi;
+area = radius * radius * PI;
+console.log("Area1:", area);
 radius = 4;
-area = radius * radius * pi;
+area = radius * radius * PI;
+console.log("Area2:", area);
