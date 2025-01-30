30d7948b1fd1aeb5728813fc0efc0d4b8ec1aa85
jkulmus <jacquelynkulmus@gmail.com>
2025-01-29 16:03:14 -0800

    added circleArea

--- week4/debugging.js
+++ week4/debugging.js
@@ -6,3 +6,14 @@ console.log("Area1:", area);
 radius = 4;
 area = radius * radius * PI;
 console.log("Area2:", area);
+
+function circleArea(radius) {
+    const area = radius * radius * PI;
+    return area;
+}
+
+area = circleArea(3);
+console.log("Area1:", area);
+
+area = circleArea(4);
+console.log("Area2:", area);
