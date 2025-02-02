010577635b60bad787219770765995a532bf1ce7
jkulmus <jacquelynkulmus@gmail.com>
2025-02-01 16:00:10 -0800

    fix js

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,43 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuButton = document.querySelector('.menu-button');
     const navMenu = document.querySelector('nav');
-
+  
     menuButton.addEventListener('click', function() {
-        navMenu.classList.toggle('hide');
+      navMenu.classList.toggle('hide');
     });
-
+  
     function handleResize() {
-        if (window.innerWidth > 1000) {
-            navMenu.classList.remove('hide');
-        } else {
-            navMenu.classList.add('hide');
-        }
+      if (window.innerWidth > 1000) {
+        navMenu.classList.remove('hide');
+      } else {
+        navMenu.classList.add('hide');
+      }
     }
-
+  
     window.addEventListener('resize', handleResize);
-    handleResize(); // imddediatly when page loads
-
-    function viewerTemplate(src, alt) {
-        return `
-        <div class="viewer">
-            <button class="close-viewer">X</button>
-            <img src="${src}" alt="${alt}">
-        </div>`;
+    handleResize(); // Call immediately when the page loads
+  
+    function viewerTemplate(pic, alt) {
+      return `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+      </div>`;
     }
-
+  
     function viewHandler(event) {
-        if (event.target.tagName === 'IMG') {
-            const imgSrc = event.target.src.split('-')[0] + '-full.jpeg';
-            const imgAlt = event.target.alt;
-            document.body.insertAdjacentHTML("afterbegin", viewerTemplate(imgSrc, imgAlt));
-
-            document.querySelector('.close-viewer').addEventListener('click', closeViewer);
-        }
+      // Create a variable to hold the element that was clicked on
+      const clickedElement = event.target;
+  
+      // Check if the clicked element is an image
+      if (clickedElement.tagName === 'IMG') {
+        // Get the src attribute from that element and split it on the "-"
+        const srcParts = clickedElement.src.split('-');
+        
+        // Construct the new image file name by adding "-full.jpeg" to the first part of the array
+        const newSrc = srcParts[0] + '-full.jpeg';
+        
+        // Insert the viewerTemplate into the top of the body element
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newSrc, clickedElement.alt));
+        
+        // Add a listener to the close button (X) that calls a function called closeViewer when clicked
+        document.querySelector('.close-viewer').addEventListener('click', closeViewer);
+      }
     }
-
+  
     function closeViewer() {
-        document.querySelector('.viewer').remove();
+      // Remove the viewer div from the DOM when clicked
+      document.querySelector('.viewer').remove();
     }
-
+  
+    // Add an event listener to the .gallery section of your HTML page
     document.querySelector('.gallery').addEventListener('click', viewHandler);
-})
+  });
