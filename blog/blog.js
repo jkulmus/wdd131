8b8e70bb003a68503322ca0352feb6a3f96df7ba
jkulmus <jacquelynkulmus@gmail.com>
2025-02-14 16:28:28 -0800

    fixed

--- blog/blog.js
+++ blog/blog.js
@@ -23,13 +23,33 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+        id: 3,
+        title: 'Belgariad Book One: Pawn of Prophecy',
+        date: 'Feb 12, 2022',
+        description: 'A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his \'Aunt Pol\' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.',
+        imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg',
+        imgAlt: 'Book cover for Pawn of Prophecy',
+        ages: '12-16',
+        genre: 'Fantasy',
+        stars: '⭐⭐⭐⭐⭐'
 	}
-]
+];
 
 document.addEventListener('DOMContentLoaded', () => {
 	const articlesSection = document.querySelector('.articles');
+
 	articles.forEach(article => {
-		const articleElement = document.createElement('article');
+		const articleElement = createArticleElement(article);
+		articlesSection.appendChild(articleElement);
+	});
+});
+
+function createArticleElement(article) {
+	const articleElement = document.createElement('article');
+	articleElement.classList.add('article');
+
 		articleElement.innerHTML = `
             <div class="article-details">
                 <time class="article-date">${article.date}</time>
@@ -39,10 +59,9 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
             <div class="article-content">
                 <h2>${article.title}</h2>
-                <img src="${article.imgSrc}" alt="${article.imgAlt}">
+                <img src="${article.imgSrc}" alt="${article.imgAlt}" onerror="this.src='placeholder.jpg'; this.onerror=null;">
                 <p>${article.description}</p>
             </div>
         `;
-		articlesSection.appendChild(articleElement);
-	});
-});
+		return articleElement;
+}
